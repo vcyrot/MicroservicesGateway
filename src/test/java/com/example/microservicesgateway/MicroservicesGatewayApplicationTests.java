553d367f595465d02b0d3dc8755a0e21e1999f45
Victoire Cyrot <76115115+vcyrot@users.noreply.github.com>
2024-06-05 21:23:46 +0200

    Gateway test

---
+++ src/test/java/com/example/microservicesgateway/MicroservicesGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicesgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
